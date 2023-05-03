46882fcc2d5302b790a0da6adc2bc85da6fcdba2
sidz <srikanth.siddhu@gmail.com>
2023-05-03 07:35:11 +0530

    many cubes

--- VD_2_0/VD_Client.cpp
+++ VD_2_0/VD_Client.cpp
@@ -1,6 +1,17 @@
 #include "VD_Client.h"
 
-
+glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -143,11 +154,20 @@ void VD_Client::Render()
     unsigned int projLoc = glGetUniformLocation(ResourceManager::GetShader("ShaderMVP").GetID(), "projection");
    
     
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    //glUniformMatrix4fv(model//Loc, 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-   
+    for (unsigned int i = 0; i < 10; i++)
+    {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        float angle = 20.0f * i;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        glDrawArrays(GL_TRIANGLES, 0, Cube.vertices.size());
+    }
   
    // glDrawArrays(GL_TRIANGLES, 0, square.vertices.size());
-    glDrawArrays(GL_TRIANGLES, 0, Cube.vertices.size());
+   
 }
--- VD_2_0/VD_Client.h
+++ VD_2_0/VD_Client.h
@@ -24,6 +24,7 @@ public:
 	 Mesh Cube;
 	 Buffer cubeRenderer;
 
+
 private:
 
 };
